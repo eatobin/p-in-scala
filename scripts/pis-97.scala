d57f9b862005c392f70d6630d187d0224e83643a
Eric Tobin <eatobin@gmail.com>
2016-11-11 14:07:17 -0600

    progress

--- scripts/pis-97.scala
+++ scripts/pis-97.scala
@@ -3,3 +3,6 @@ println(formatArgs(Array("zero", "one", "two")))
 
 val res = formatArgs(Array("zero", "one", "two"))
 println(assert(res == "zero\none\ntwo"))
+//println(assert(res == "zero\none\ntwoX", "Does not work!"))
+
+// :load /home/eric/scala_projects/p-in-scala/scripts/pis-97.scala
