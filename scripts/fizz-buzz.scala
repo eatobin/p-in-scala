ad6a4c90d3d19ae58592e78bd7b07ed58600b90e
Eric Tobin <eatobin@gmail.com>
2017-11-13 11:15:34 -0600

    practice

--- scripts/fizz-buzz.scala
+++ scripts/fizz-buzz.scala
@@ -12,5 +12,18 @@ def fizzBuzz2(n: Int): String = n match {
   case x => x.toString
 }
 
+def fizzBuzz3(n: Int): String = {
+  (n % 3, n % 5, n % 15) match {
+    case (0, 0, 0) => "FizzBuzz"
+    case (0, _, _) => "Fizz"
+    case (_, 0, _) => "Buzz"
+    case _ => n.toString
+  }
+
+}
+
 //println((1 to 100).map(fizzBuzz))
-println((1 to 100).map(fizzBuzz2))
+//println((1 to 100).map(fizzBuzz2))
+(7 to 16).map(fizzBuzz2)
+(10 to 20) map fizzBuzz2
+(306 to 317).map(fizzBuzz3)
