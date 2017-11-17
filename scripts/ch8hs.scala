8d4cd334632cb070b22f190e8357ad2a2c4e2a85
Eric Tobin <eatobin@gmail.com>
2017-11-17 14:51:23 -0600

    ch8 progress!

--- scripts/ch8hs.scala
+++ scripts/ch8hs.scala
@@ -17,3 +17,30 @@ def three(): Int =
 
 //scala> three
 //res9: Int = 9
+
+def incTimes(t: Int)(s: Int): Int =
+  (t, s) match {
+    case (0, n) => n
+    case (i, n) =>
+      inc(incTimes(i - 1)(n))
+  }
+
+//scala> incTimes(3)(6)
+//res11: Int = 9
+//scala> incTimes(3)(16)
+//res12: Int = 19
+//scala> incTimes(0)(16)
+//res13: Int = 16
+//scala> incTimes(16)(0)
+//res14: Int = 16
+
+def applyTimes(t: Int)(f: Int => Int)(s: Int): Int =
+  (t, f, s) match {
+    case (0, _, n) => n
+    case (i, g, n) => g(applyTimes(i - 1)(g)(n))
+  }
+
+//scala> applyTimes(0)(inc)(8)
+//res20: Int = 8
+//scala> applyTimes(4)(inc)(8)
+//res21: Int = 12
