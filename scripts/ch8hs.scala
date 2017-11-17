14f612eff9380b2f45be0da17788423edad8f389
Eric Tobin <eatobin@gmail.com>
2017-11-17 15:09:38 -0600

    ch8 progress!

--- scripts/ch8hs.scala
+++ scripts/ch8hs.scala
@@ -44,3 +44,37 @@ def applyTimes(t: Int)(f: Int => Int)(s: Int): Int =
 //res20: Int = 8
 //scala> applyTimes(4)(inc)(8)
 //res21: Int = 12
+
+def fibonacci(n: Int): Int =
+  n match {
+    case 0 => 0
+    case 1 => 1
+    case x =>
+      fibonacci(x - 1) + fibonacci(x - 2)
+  }
+
+//scala> fibonacci(6)
+//res22: Int = 8
+//scala> fibonacci(0)
+//res23: Int = 0
+//scala> fibonacci(1)
+//res24: Int = 1
+//scala> fibonacci(2)
+//res25: Int = 1
+//scala> fibonacci(22)
+//res26: Int = 17711
+
+def dividedBy(num: Int)(denom: Int): (Int, Int) = {
+  @annotation.tailrec
+  def go(n: Int)(d: Int)(count: Int): (Int, Int) = {
+    if (n < d) (count, n)
+    else go(n - d)(d)(count + 1)
+  }
+
+  go(num)(denom)(0)
+}
+
+//scala> dividedBy(20)(4)
+//res28: (Int, Int) = (5,0)
+//scala> dividedBy(21)(4)
+//res29: (Int, Int) = (5,1)
